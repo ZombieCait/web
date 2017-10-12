db08b756584c7277d9df610d0d7c15c4d817f10e
ZombieCait <caitzombie@gmail.com>
2017-10-13 02:37:48 +0500

    lab01

---
+++ lab01/lab01/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab01.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Home()
+        {
+            return Home();
+        }
+        public IActionResult About()
+        {
+            return About();
+        }
+        public IActionResult Contact()
+        {
+            return Contact();
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
