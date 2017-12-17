a9fd9832305bf0625d995a53f33bfa82aa5e1e4b
ZombieCait <caitzombie@gmail.com>
2017-12-17 16:45:22 +0500

    lab_02

---
+++ lab_02/lab_02/Models/SMTPViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab_02.Models
+{
+    public class SMTPViewModel
+    {
+        public static string Host { get; set; }
+        public static int Port { get; set; }
+        public static string EmailAddress { get; set; }
+        public static string Login { get; set; }
+        public static string Password { get; set; }
+       
+        public static void LoadSettings(Microsoft.Extensions.Configuration.IConfiguration configuration)
+        {
+            Host = configuration["SMTP:Host"];
+            Port = Convert.ToInt32(configuration["SMTP:Port"]);
+            EmailAddress = configuration["SMTP:Email"];
+            Login = configuration["SMTP:Login"];
+            Password = configuration["SMTP:Password"];
+        }
+        
+    }
+}
